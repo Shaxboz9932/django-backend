e18f4122185ae848056382866db46da9f74c8e44
unknown <shaxboz9932@gmail.com>
2026-03-04 10:26:05 +0500

    add debug-toolbar

--- config/urls.py
+++ config/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path("cart/", include("cart.urls")),
     path("wishlist/", include("wishlist.urls")),
     path("order/", include("order.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + debug_toolbar_urls()
 
 
